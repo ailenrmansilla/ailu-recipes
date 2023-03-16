a13d99795a2357b6958cdd775de963c23c487dce
ailenrmansilla <ailen.r.mansilla@gmail.com>
2023-03-16 18:48:09 -0400

    css and images folder created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { createElement } from './utils';
 
 
 function App() {
-  const header = createElement('h1', {textContent: 'Ailu Recipes', className: 'heading'});
-
+  const appTitle = createElement('h1', {textContent: 'Ailu Recipes', className: 'heading'});
+  const header = createElement('header',{}, [appTitle])
   const main = createElement('main', {});
 
-  return createElement('div', {}, [header, main]); 
+  const copyright = createElement('span',{textContent: `Copyright &copy; ${new Date().getFullYear()}`, })
+  const footer = createElement('footer',{}, [copyright]);
+
+  return createElement('div', {}, [header, main, footer]); 
 }
 
 export default App; // it returns that last createElement (the whole div)
